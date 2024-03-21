9c0fca14262ac0035c94c16d99c688c924cb3dfa
Cruzzardo <can71695@udelasalle.edu.mx>
2024-03-20 19:30:28 -0600

    Updates

---
+++ index.js
@@ -0,0 +1,79 @@
+import * as deepar from "deepar";
+import Carousel from "./carousel.js";
+
+// Log the version. Just in case.
+console.log("Deepar version: " + deepar.version);
+
+// Top-level await is not supported.
+// So we wrap the whole code in an async function that is called immediatly.
+(async function () {
+  // Get the element you want to place DeepAR into. DeepAR will inherit its width and height from this and fill it.
+  const previewElement = document.getElementById("ar-screen");
+
+  // trigger loading progress bar animation
+  const loadingProgressBar = document.getElementById("loading-progress-bar");
+  loadingProgressBar.style.width = "100%";
+
+  // All the effects are in the public/effects folder.
+  // Here we define the order of effect files.
+  const effectList = [
+    "effects/ariat.deepar",
+    "effects/shoe.deepar",
+    "effects/Stallone.deepar",
+    "effects/Vendetta_Mask.deepar",
+    "effects/viking_helmet.deepar",
+  ];
+
+  let deepAR = null;
+
+  // Initialize DeepAR with an effect file.
+  try {
+    deepAR = await deepar.initialize({
+      licenseKey: "66255220b032d1783b464a0ff3dca9a3882397235ed158e1113b0e8e3b8c1cf8091b4eb25f52e98d",
+      previewElement,
+      effect: effectList[0],
+      // Removing the rootPath option will make DeepAR load the resources from the JSdelivr CDN,
+      // which is fine for development but is not recommended for production since it's not optimized for performance and can be unstable.
+      // More info here: https://docs.deepar.ai/deepar-sdk/platforms/web/tutorials/download-optimizations/#custom-deployment-of-deepar-web-resources
+      rootPath: "./deepar-resources",
+      additionalOptions: {
+        cameraConfig: {
+          facingMode: 'environment'  // uncomment this line to use the rear camera
+        },
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    document.getElementById("loading-screen").style.display = "none";
+    document.getElementById("permission-denied-screen").style.display = "block";
+    return;
+  }
+
+  // Hide the loading screen.
+  document.getElementById("loading-screen").style.display = "none";
+  document.getElementById("ar-screen").style.display = "block";
+
+  window.effect = effectList[0];
+
+  const glassesCarousel = new Carousel("carousel");
+  glassesCarousel.onChange = async (value) => {
+    const loadingSpinner = document.getElementById("loading-spinner");
+
+    if (window.effect !== effectList[value]) {
+      loadingSpinner.style.display = "block";
+      await deepAR.switchEffect(effectList[value]);
+      window.effect = effectList[value];
+    }
+    loadingSpinner.style.display = "none";
+  };
+})();
+
+// Register for a collback that lets you know when feet are detected.
+deepAR.callbacks.onFeetTracked = (leftFoot, rightFoot) => {
+  const feetText = document.getElementById("feet-text");
+  // Hide the text when the feet are first detected.
+  if(leftFoot.detected || rightFoot.detected) {
+    feetText.style.display = "none";
+    deepAR.callbacks.onFeetTracked = undefined; // Unregister from the callback.
+  }
+};
